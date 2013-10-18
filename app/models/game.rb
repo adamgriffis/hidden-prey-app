9f73e5f5492fcf7ee70f4482226a16f5fd57dab4
Guy Hoskinson <guy.hoskinson@gmail.com>
2013-10-18 00:09:37 -0400

    Tests passing, db settings & game model tweaks

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,5 @@
 class Game < ActiveRecord::Base
 	has_many :players
+
+	validates :name, uniqueness: true, presence: true, :case_sensitive => false
 end
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,5 +1,28 @@
 require 'spec_helper'
 
 describe Game do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+	before(:each) do
+		@attr = {
+			:name => "myGame",
+		}
+	end
+
+	it "should create a new instance given a valid attribute" do
+		Game.create!(@attr)
+	end
+
+	it "should reject duplicate name" do
+		Game.create!(@attr)
+		game_with_duplicate_name = Game.new(@attr)
+		game_with_duplicate_name.should_not be_valid
+	end
+=begin
+	it "should reject name identical up to case" do
+		upcased_name = @attr[:name].upcase
+		Game.create!(@attr.merge(:name => upcased_name))
+		game_with_duplicate_name = Game.new(@attr)
+		game_with_duplicate_name.should_not be_valid
+	end
+=end
 end
